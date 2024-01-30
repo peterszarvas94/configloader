1db5c0da0e36b731808fcdd5b18e277b83fca6c5
Szarvas Péter <peterszarvas94@gmail.com>
2024-01-30 18:26:56 +0100

    add: set env file

---
+++ README.md
@@ -0,0 +1,41 @@
+# Simple env loader
+
+It checks your environment variables at build time, based on a predefined struct
+
+## Install
+
+```shell
+go get github.com/peterszarvas95/envloader@v1.0.5
+```
+
+## Usage
+
+```go
+package main
+
+import (
+  "fmt"
+  "os"
+
+  "github.com/peterszarvas94/envloader"
+)
+
+
+func main() {
+  type Config struct {
+    DB_NAME string
+  }
+  var config Config
+
+  // optional
+  envloader.File(".env")
+
+  err := envloader.Loader(&config)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+
+  fmt.Println(config.DB_NAME)
+}
+```
--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package envloader
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"reflect"
 	"strings"
 )
 
+var envPath string
+
+func File(path string) {
+	envPath = path
+}
+
 func Loader(variables interface{}, keys ...string) error {
+	if envPath != "" {
+		err := loadEnvFromFile(envPath)
+		if err != nil {
+			return err
+		}
+	}
+
 	v := reflect.ValueOf(variables)
 	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
 		return fmt.Errorf("initEnv expects a pointer to a struct")
@@ -31,6 +45,49 @@ func Loader(variables interface{}, keys ...string) error {
 		}
 
 		field.SetString(envVarValue)
+	}
+
+	return nil
+}
+
+func loadEnvFromFile(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	for _, line := range lines {
+		// Skip empty lines and comments
+		if len(line) == 0 || line[0] == '#' {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			return fmt.Errorf("Invalid line: %s", line)
+		}
+
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		// strip quotes
+		if len(value) >= 2 && value[0] == '"' && value[len(value)-1] == '"' {
+			value = value[1 : len(value)-1]
+		}
+
+		os.Setenv(key, value)
 	}
 
 	return nil
